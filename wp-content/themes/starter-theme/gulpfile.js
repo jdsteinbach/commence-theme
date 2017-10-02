60b2edf249971c4265a3a9d75f852f5e663eaa35
James Steinbach <james.steinbach@dockyard.com>
2017-10-02 15:48:51 -0600

    move gulp stuff into theme folder

--- wp-content/themes/starter-theme/gulpfile.js
+++ wp-content/themes/starter-theme/gulpfile.js
@@ -3,18 +3,17 @@
 
   /**
    * Required node plugins
-   * NOTE: Be sure to run `npm install` in order to use all the dependencies mentioned in this `gulpfile`
    */
-  var gulp         = require('gulp');
-  var glob         = require('glob');
-  var del          = require('del');
-  var browserSync  = require('browser-sync');
-  var reload       = browserSync.reload;
-  var $            = require('gulp-load-plugins')();
-  var postcss      = require('gulp-postcss');
-  var autoprefixer = require('autoprefixer');
-  var cssnano      = require('cssnano');
-  var order        = require('postcss-ordered-values');
+  var gulp        = require('gulp');
+  var glob        = require('glob');
+  var del         = require('del');
+  var browserSync = require('browser-sync').create();
+  var reload      = browserSync.reload;
+  var $           = require('gulp-load-plugins')();
+  var postcss     = require('gulp-postcss');
+  var prefix      = require('autoprefixer');
+  var cssnano     = require('cssnano');
+
 
   /**
    * Set up prod/dev tasks
@@ -23,25 +22,14 @@
 
 
   /**
-   * Set up project variables
-   */
-  var pkg           = require('./package.json');
-  // The `shortName` in package.json should be the theme dir name
-  var _theme_name   = pkg.shortName.toLowerCase();
-
-
-  /**
    * Set up file paths
    */
-  var _bower_dir    = 'bower_components';
-  var _theme_dir    = 'wp-content/themes/' + _theme_name;
-  var _assets_dir   = _theme_dir + '/assets';
-  var _src_dir      = _assets_dir + '/src';
-  var _dist_dir     = _assets_dir + '/dist';
-  var _dev_dir      = _assets_dir + '/dev';
-  var _img_src_dir = _src_dir + '/img';
-  var _img_dir      = _assets_dir + '/img';
-  var _build_dir    = (is_prod) ? _dist_dir : _dev_dir;
+  var _assets_dir     = 'assets';
+  var _src_dir        = _assets_dir + '/src';
+  var _dist_dir       = _assets_dir + '/dist';
+  var _dev_dir        = _assets_dir + '/dev';
+  var _build_dir      = (is_prod) ? _dist_dir : _dev_dir;
+  var _bower_dir      = 'bower_components';
 
 
   /**
@@ -136,7 +124,7 @@
           .on( 'error', errorAlert )
           .pipe(
             $.notify({
-              message:  (is_prod) ? dir + ' scripts have been compiled and minified' : dir + ' dev scripts have been compiled',
+              message: dir + ' scripts have been compiled',
               onLast:   true
             })
           );
@@ -150,18 +138,15 @@
    */
   gulp.task('styles', function() {
     var _sass_opts = {
-      outputStyle:    'expanded',
+      outputStyle:  is_prod ? 'compressed' : 'expanded',
       sourceComments: !is_prod
     };
 
-    var ap_options = autoprefixer({
-      cascade:  false
-    });
-
-    var _postcss = [
-      ap_options,
-      order(),
+    var _postcss_opts = [
+      prefix({browsers: ['last 3 versions']})
     ];
+
+    if ( is_prod ) _postcss_opts.push(cssnano());
 
     gulp.src(_src_dir + '/scss/style.scss')
       .pipe( $.plumber({ errorHandler: errorAlert }) )
@@ -176,8 +161,7 @@
         );
       })
       .pipe( is_prod ? $.rename({ suffix: '.min' }) : $.util.noop() )
-      .pipe( postcss(_postcss) )
-      .pipe( is_prod ? postcss([cssnano()]) : $.util.noop() )
+      .pipe( postcss(_postcss_opts) )
       .pipe( gulp.dest(_build_dir) )
       .pipe( reload({stream:true}) )
       .on( 'error', errorAlert )
@@ -202,58 +186,31 @@
       .pipe( $.sassLint.failOnError() );
   });
 
-  /**
-   * Minimizes all the images
-   */
-  gulp.task('images', function() {
-    var _options = {
-      progressive: true,
-      svgoPlugins: [{
-        removeViewBox: false,
-        removeHiddenElems: false
-      }]
-    };
-    return gulp.src(_img_src_dir + '/**/*.{png,jpg,jpeg,gif,svg}')
-      .pipe( $.changed(_img_dir) )
-      .pipe( $.imagemin(_options) )
-      .pipe( gulp.dest(_img_dir) )
-      .pipe(
-        $.notify({
-          message:  'Images have been compressed',
-          onLast:   true
-        })
-      );
-  });
-
 
   /**
    * Builds for distribution (staging or production)
    */
   gulp.task('build', [
     'clean',
-    'lint',
-    'scripts',
-    'styles',
-    'images'
+    'styles'
   ]);
 
 
   /**
    * Builds assets and reloads the page when any php, html, img or dev files change
    */
-  gulp.task('watch', function() {
-    browserSync({
-      proxy: pkg.name.toLowerCase() + '.dev',
-      notify: true,
-      port: 3200
+  gulp.task('watch', ['build'], function() {
+    browserSync.init({
+      server: {
+        baseDir: './'
+      },
+      notify: true
     });
+
     gulp.watch( _src_dir + '/scss/**/*', ['styles'] );
     gulp.watch( _src_dir + '/js/**/*', ['scripts'] );
-    gulp.watch( _theme_dir + '/**/*.php' ).on('change', reload );
-    gulp.watch( _theme_dir + '/**/*.html' ).on('change', reload );
-    gulp.watch( _img_src_dir + '/**/*.{png,jpg,jpeg,gif,svg}', ['images'] );
+    gulp.watch( './**/*.html' ).on('change', reload );
   });
-
 
   /**
    * Backup default task just triggers a build
